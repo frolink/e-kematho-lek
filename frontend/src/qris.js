0822b7a0ecb8997861e079d4ef4807ca178ba626
frolink <54199937+frolink@users.noreply.github.com>
2025-11-23 16:22:15 -0800

    Delete frontend directory

--- frontend/src/qris.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const BACKEND_URL = 'http://127.0.0.1:5000';
-
-async function payViaQris(amount, metadata) {
-  const res = await axios.post(`${BACKEND_URL}/qris/pay`, { amount, metadata });
-  window.open(res.data.qris_url, "_blank");
-}
-
-export { payViaQris };
